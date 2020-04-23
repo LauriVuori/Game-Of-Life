d0ccab928c5eefffc1a07e1b8c1cd277d63758a4
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-04-23 13:23:48 +0300

    Update layout, and description

--- GoL/src/game_of_life.c
+++ GoL/src/game_of_life.c
@@ -72,7 +72,10 @@
  @author Lauri Vuori
  @copyright GNU Public License.
 
- Game of liffe asddasdasdas....
+ "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.<br>
+ It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. 
+ One interacts with <br> the Game of Life by creating an initial configuration and observing how it evolves. It is Turing complete and can simulate <br>
+ a universal constructor or any other Turing machine." (Wikipedia 2020.)
  */
 /**
  @fn int main(void)
